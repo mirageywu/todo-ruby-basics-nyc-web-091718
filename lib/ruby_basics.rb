cf10092963026e714c6f2a963d165fbc819f104f
Yashih Wu <yashih.wu@gmail.com>
2018-09-08 05:52:53 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,18 +10,17 @@ def argue(argument)
   argument
 end
 
-def greeting
-
+def greeting(greeting,name)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  type
 end
